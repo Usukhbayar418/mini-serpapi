0972240409be015fdd5f19e2f4c9659c4ff3ed14
osohoob <164154250+Usukhbayar418@users.noreply.github.com>
2026-05-13 20:37:56 +0800

    deploy: generate credentials and allow onrender.com hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,10 +67,10 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    /.*\.onrender\.com/,     # Allow requests from example.com
+    "localhost",             # Allow requests from localhost
+    ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
